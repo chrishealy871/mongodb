68b7eb22603d7b1515daa124d570e3d7854fa250
Chris Healy <chrishealy@Chriss-MacBook-Air.local>
2017-02-22 12:34:43 +0000

    Updated Search

--- pymongo_m.py
+++ pymongo_m.py
@@ -1,11 +1,16 @@
 import pymongo
+
+
 def mongo_connect():
-   try:
-       conn = pymongo.MongoClient()
-       print "Mongo is connected!"
-       return conn
-   except pymongo.errors.ConnectionFailure, e:
-       print "Could not connect to MongoDB: %s" % e
+    try:
+        conn = pymongo.MongoClient()
+        print "Mongo is connected!"
+        return conn
+    except pymongo.errors.ConnectionFailure, e:
+        print "Could not connect to MongoDB: %s" % e
+
+
 conn = mongo_connect()
 db = conn['twitter_stream']
-print conn.database_names()  # [u'local', u'test']
+coll = db.my_collection
+print db.collection_names()# [u'local', u'test']
